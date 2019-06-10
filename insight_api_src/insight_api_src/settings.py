806977d76184bfbcee858fab1f98bed0b80150da
jiananarthurli <reudnetwork@gmail.com>
2019-06-10 17:51:18 -0400

    extractor, recommender and vectorizer apps are added.

--- insight_api_src/insight_api_src/settings.py
+++ insight_api_src/insight_api_src/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'vectorizer.apps.VectorizerConfig',
+    'extractor.apps.ExtractorConfig',
+    'recommender.apps.RecommenderConfig',
 ]
 
 MIDDLEWARE = [
