e84c2e5dc5420efc078868ee0740cef1ffabf262
jiananarthurli <reudnetwork@gmail.com>
2019-06-10 19:51:04 -0400

    Setup the api to take a wiki topic and return an averaged embedding vector

--- insight_api_src/insight_api_src/urls.py
+++ insight_api_src/insight_api_src/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+from recommender.views import recommender
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+]
+
+urlpatterns += [
+    url(r'^submission/$', recommender, name='submission'),
 ]
--- insight_api_src/recommender/views.py
+++ insight_api_src/recommender/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from extractor.views import wikiExtractor
+from vectorizer.views import article2Vec
+import json
 
 # Create your views here.
+
+def recommender(request):
+
+    wiki_topic = request.GET['wiki_topic']
+
+    wiki_text = wikiExtractor(wiki_topic)
+    wiki_vector = article2Vec(wiki_text)
+    response = json.dumps(list(wiki_vector))
+
+    return HttpResponse(response)
