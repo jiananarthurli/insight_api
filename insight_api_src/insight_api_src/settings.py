ae84063a7f381f5582870dd49106d9ce1efe823f
jiananarthurli <reudnetwork@gmail.com>
2019-06-24 13:24:23 -0400

    Turned off debugging

--- insight_api_src/insight_api_src/settings.py
+++ insight_api_src/insight_api_src/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = ')b*mjn!g5d=m+_v85(@kr7y8=qtn)k(+#5#eeaj5zy%wpcj(sz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
