15376897c11f60e0b10a1a6c0852d47ffe0a4347
jiananarthurli <reudnetwork@gmail.com>
2019-07-04 16:10:09 -0400

    Added Python scripts for PostgreSQL update.

---
+++ Python/update_event_list.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import json
+from sqlalchemy import create_engine
+
+#path = '/Users/arthurli/Documents/Coding/InsightProject/insight_api/insight_api_data/'
+path  = '/home/arthurli/insight_api/insight_api_data'
+filename = 'event_list.txt'
+
+with open(path + filename, 'r') as f:
+    event_list_json = json.load(f)
+
+event_list = pd.DataFrame(event_list_json)
+
+engine = create_engine('postgresql://arthurli:chrome_insight@localhost:5432/insight', echo=False)
+event_list.to_sql(con=engine, name='event_list', if_exists='replace', chunksize=1000)
